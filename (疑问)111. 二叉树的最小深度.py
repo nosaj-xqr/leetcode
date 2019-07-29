13671304516d663d97edf5325aaa579a48fabe88
nosaj-xqr <52945232+nosaj-xqr@users.noreply.github.com>
2019-07-29 15:05:23 +0800

    Create (疑问)111. 二叉树的最小深度.py

---
+++ (疑问)111. 二叉树的最小深度.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+'''
+讨论区题解，没太搞明白
+'''
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        if not root.right:
+            return self.minDepth(root.left) + 1
+        return min(self.minDepth(root.right),self.minDepth(root.left)) + 1
